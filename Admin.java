b08782d2bacfe691be2ecbbc6fd826b7bdc50dc4
Gukelaj <njuul7@gmail.com>
2017-05-02 11:16:44 +0200

    meh2

---
+++ Admin.java
@@ -0,0 +1,22 @@
+public class Admin
+{
+    private String name;
+    private String password;
+
+    public Admin(String name, String password)
+    {
+        this.name = name;
+        this.password = password;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public String getPassword()
+    {
+        return password;
+    }
+
+}
